64c66ace408e964110c59d3f14a4e08b463babf7
Pedro Batista <pedro.batista@diconium.com>
2024-10-30 17:45:45 +0000

    adding main

---
+++ src/main.c
@@ -0,0 +1,8 @@
+// src/main.c
+#include <stdio.h>
+#include "submodule.h"  // Include header from submodule
+
+int main() {
+    hello_from_submodule();  // Call the function from the submodule
+    return 0;
+}
