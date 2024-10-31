dfb817fe2d04e3d96414056d1f6f92a52f1b3209
Pedro Batista <pedro.batista@diconium.com>
2024-10-31 01:58:50 +0000

    Changing main

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "submodule/src/submodule.h"  // Include header from submodule
 
 int main() {
+// a sample change
     hello_from_submodule();  // Call the function from the submodule
     return 0;
 }
