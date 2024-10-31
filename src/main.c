d086642a78ca66c2903ab261192e4cc59597fe88
Pedro Batista <pedro.batista@diconium.com>
2024-10-31 00:28:50 +0000

    Changing main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 // src/main.c
 #include <stdio.h>
-#include "submodule.h"  // Include header from submodule
+#include "submodule/src/submodule.h"  // Include header from submodule
 
 int main() {
     hello_from_submodule();  // Call the function from the submodule
