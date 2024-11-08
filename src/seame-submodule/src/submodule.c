bad77fd467a57363fc8789b2681a01f2839bc0ae
Antonio Ferreira <af.myhouse@gmail.com>
2024-11-08 13:15:46 +0000

    removed forked submodule

---
+++ src/seame-submodule/src/submodule.c
@@ -0,0 +1,7 @@
+// submodule-project/submodule.c
+#include <stdio.h>
+#include "submodule.h"
+
+void hello_from_submodule() {
+    printf("Hello from the submodule v4!\n");
+}
---
+++ src/seame-submodule/src/submodule.h
@@ -0,0 +1,7 @@
+// submodule-project/submodule.h
+#ifndef SUBMODULE_H
+#define SUBMODULE_H
+
+void hello_from_submodule();
+
+#endif
